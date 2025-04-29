afec923dc8ab366dcef918345c31a0f198394292
NoJusticeItaly <malapolizia00@gmail.com>
2025-04-29 14:21:16 +0200

    first commit

---
+++ table.js
@@ -0,0 +1,20 @@
+fetch("archivio.JSON")
+.then(res => res.json())
+.then(data => generateTable(data))
+
+function generateTable(data){
+  $('#main-table').DataTable( {
+    data: data,
+    columns: [
+      { data: 'name', title: "Evento" },
+      { data: 'place', title: "Luogo" },
+      { data: 'when', title: "Data" },
+      { data: 'who', title: "Vittima" },
+      { data: 'age', title: "Età" },
+      { data: 'nationality', title: "Nazionalità" },
+      { data: 'description', title: "Breve Descrizione" },
+      { data: 'sentence', title: "Giustizia" },
+      { data: 'link', title: "Articolo", render: DataTable.render.hyperLink() },
+    ]
+  } );
+}
